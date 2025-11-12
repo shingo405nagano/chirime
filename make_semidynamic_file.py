c1ce20e124bd6ea9af1d225664b9144ff7a62452
shingo405nagano <makishin.nagano@gmail.com>
2025-11-12 12:20:41 +0000

    [Refactor] セミダイナミック補正ファイルのURL管理をクラスから辞書に変更

--- make_semidynamic_file.py
+++ make_semidynamic_file.py
@@ -1,6 +1,8 @@
 """
 このモジュールは国土地理院で公開されている、セミダイナミック補正を行うための
 パラメーターファイルをダウンロードした後に、後処理する為のスクリプトです。
+パラメーターファイルは各年ごとに公開されており、現在は2009年から2025年まで対応しています。
+新しいファイルが公開された場合は、対応するURLを追加することでダウンロード可能です。
 
 パラメーターファイル：https://www.gsi.go.jp/sokuchikijun/semidyna.html#file
 """
@@ -9,32 +11,30 @@ import os
 import subprocess
 import tempfile
 import zipfile
-from dataclasses import dataclass
 
 import pandas as pd
 
 from chirime import geomesh
 
-
-@dataclass
-class SemiDynaParamUrls:
-    y2009 = "https://www.gsi.go.jp/common/000185050.zip"
-    y2010 = "https://www.gsi.go.jp/common/000185049.zip"
-    y2011 = "https://www.gsi.go.jp/common/000185048.zip"
-    y2012 = "https://www.gsi.go.jp/common/000185047.zip"
-    y2013 = "https://www.gsi.go.jp/common/000185046.zip"
-    y2014 = "https://www.gsi.go.jp/common/000185045.zip"
-    y2015 = "https://www.gsi.go.jp/common/000185044.zip"
-    y2016 = "https://www.gsi.go.jp/common/000185043.zip"
-    y2017 = "https://www.gsi.go.jp/common/000186321.zip"
-    y2018 = "https://www.gsi.go.jp/common/000198965.zip"
-    y2019 = "https://www.gsi.go.jp/common/000224044.zip"
-    y2020 = "https://www.gsi.go.jp/common/000221430.zip"
-    y2021 = "https://www.gsi.go.jp/common/000239301.zip"
-    y2022 = "https://www.gsi.go.jp/common/000248856.zip"
-    y2023 = "https://www.gsi.go.jp/common/000248852.zip"
-    y2024 = "https://www.gsi.go.jp/common/000256608.zip"
-    y2025 = "https://www.gsi.go.jp/common/000268763.zip"
+params_urls = {
+    "2009": "https://www.gsi.go.jp/common/000185050.zip",
+    "2010": "https://www.gsi.go.jp/common/000185049.zip",
+    "2011": "https://www.gsi.go.jp/common/000185048.zip",
+    "2012": "https://www.gsi.go.jp/common/000185047.zip",
+    "2013": "https://www.gsi.go.jp/common/000185046.zip",
+    "2014": "https://www.gsi.go.jp/common/000185045.zip",
+    "2015": "https://www.gsi.go.jp/common/000185044.zip",
+    "2016": "https://www.gsi.go.jp/common/000185043.zip",
+    "2017": "https://www.gsi.go.jp/common/000186321.zip",
+    "2018": "https://www.gsi.go.jp/common/000198965.zip",
+    "2019": "https://www.gsi.go.jp/common/000224044.zip",
+    "2020": "https://www.gsi.go.jp/common/000221430.zip",
+    "2021": "https://www.gsi.go.jp/common/000239301.zip",
+    "2022": "https://www.gsi.go.jp/common/000248856.zip",
+    "2023": "https://www.gsi.go.jp/common/000248852.zip",
+    "2024": "https://www.gsi.go.jp/common/000256608.zip",
+    "2025": "https://www.gsi.go.jp/common/000268763.zip",
+}
 
 
 def get_semidyna_param(url: str) -> pd.DataFrame:
@@ -96,28 +96,7 @@ def get_semidyna_param(url: str) -> pd.DataFrame:
 
 if __name__ == "__main__":
     dirname = os.path.join(os.path.dirname(__file__), "chirime/data")
-    years = list(range(2009, 2026))
-    urls = [
-        SemiDynaParamUrls.y2009,
-        SemiDynaParamUrls.y2010,
-        SemiDynaParamUrls.y2011,
-        SemiDynaParamUrls.y2012,
-        SemiDynaParamUrls.y2013,
-        SemiDynaParamUrls.y2014,
-        SemiDynaParamUrls.y2015,
-        SemiDynaParamUrls.y2016,
-        SemiDynaParamUrls.y2017,
-        SemiDynaParamUrls.y2018,
-        SemiDynaParamUrls.y2019,
-        SemiDynaParamUrls.y2020,
-        SemiDynaParamUrls.y2021,
-        SemiDynaParamUrls.y2022,
-        SemiDynaParamUrls.y2023,
-        SemiDynaParamUrls.y2024,
-        SemiDynaParamUrls.y2025,
-    ]
-
-    for url, year in zip(urls, years, strict=False):
+    for year, url in params_urls.items():
         output_path = os.path.join(dirname, f"SemiDyna{year}.csv")
         if os.path.exists(output_path):
             print(
